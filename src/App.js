074178cfca692c058dfd77030fd92c24750b91f5
Joe Doyle <joe@woebot.io>
2018-09-07 09:46:35 -0700

    Fixes and cleanup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { hot } from 'react-hot-loader';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { hot } from "react-hot-loader";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-import Header from './components/Header';
-import EntryList from './components/EntryList';
+import Header from "./components/Header";
+import EntryList from "./components/EntryList";
 
 const App = () => {
   return (
-	<div class='container-fluid'>
-		<Header />
-		<div class="row main-title">
-			<div class="container">
-			  <h1>Lesson: All or nothing <button class="empty-solid"><i class="fas fa-pencil-alt"></i></button></h1>
-			</div>
-		</div>
+    <div className="container-fluid">
+      <Header />
+      <div className="row main-title">
+        <div className="container">
+          <h1>
+            Lesson: All or nothing{" "}
+            <button className="empty-solid">
+              <i className="fas fa-pencil-alt" />
+            </button>
+          </h1>
+        </div>
+      </div>
 
-		<div class="container">
-		  <button class="back col-md-3 col-sm-12">
-			<i class="fas fa-arrow-left"></i> Back to lessons list
-		  </button>
+      <div className="container">
+        <button className="back col-md-3 col-sm-12">
+          <i className="fas fa-arrow-left" /> Back to lessons list
+        </button>
 
-		  <div class="buttons">
-			<button class="main col-md-3 col-sm-12">
-			  <i class="fas fa-plus-circle"></i> Add a new entry
-			</button>
+        <div className="buttons">
+          <button className="main col-md-3 col-sm-12">
+            <i className="fas fa-plus-circle" /> Add a new entry
+          </button>
 
-			<TextField
-			  id="search"
-			  className='col-md-9 col-sm-12'
-			  label="Search"
-			  margin="normal"
-			/>
-		  </div>
+          <TextField
+            id="search"
+            className="col-md-9 col-sm-12"
+            label="Search"
+            margin="normal"
+          />
+        </div>
 
-		  <EntryList />
-		</div>
-	</div>
+        <EntryList />
+      </div>
+    </div>
   );
 };
 
-export default hot(module)(App)
+export default hot(module)(App);
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,68 +1,72 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import style from "../styles/entry.css";
 
-import * as allornothing from '../allornothing.json';
+import * as allornothing from "../allornothing.json";
 
 class EntryList extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       list: Object.values(allornothing)
-    }
+    };
 
-	this.renderReplies = this.renderReplies.bind(this)
+    this.renderReplies = this.renderReplies.bind(this);
   }
 
   renderReplies(replies) {
-    return replies.map(reply => {
-        return (
-            <div class="entry-body col-12">
-				<button class="back">{ reply }</button>
-			</div>
-        )
-    })
+    if (!replies) {
+      return <div />;
+    }
+
+    return replies.map((reply, idx) => {
+      return (
+        <div className="entry-body col-12" key={idx}>
+          <button className="back">{reply}</button>
+        </div>
+      );
+    });
   }
-  
+
   render() {
     return (
-      <div class="row" id="entries-list">
-		{this.state.list.map(entry => {
-          return (
-			<div class="col-md-4 col-sm-12">
-			  <div class="entry col-12">
-				<div class="row">
-				  <div class="entry-header col-12">
-					<div class="entry-id">
-					  <span>{entry.id}</span>
-					</div>
-					<p>{entry.text}</p>
-				  </div>
+      <div className="row" id="entries-list">
+        {this.state.list.map(
+          function(entry, idx) {
+            return (
+              <div className="col-md-4 col-sm-12" key={idx}>
+                <div className="entry col-12">
+                  <div className="row">
+                    <div className="entry-header col-12">
+                      <div className="entry-id">
+                        <span>{entry.id}</span>
+                      </div>
+                      <p>{entry.text}</p>
+                    </div>
 
-				  { this.renderReplies(entry.replies) }
+                    {this.renderReplies(entry.replies)}
 
-				  <div class="entry-manage col-12">
-					<div class="row">
+                    <div className="entry-manage col-12">
+                      <div className="row">
+                        <span className="manage-button">
+                          <i className="fas fa-comments" /> Add new answer
+                        </span>
 
-					  <span class="manage-button">
-						<i class="fas fa-comments"></i> Add new answer
-					  </span>
-
-					  <span class="manage-button">
-						<i class="fas fa-pencil-alt"></i> Modify
-					  </span>
-					</div>
-				  </div>
-				</div>
-			  </div>
-			</div>
-          )
-        })}
-	  </div>
-    )
+                        <span className="manage-button">
+                          <i className="fas fa-pencil-alt" /> Modify
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          }.bind(this)
+        )}
+      </div>
+    );
   }
 }
 
-export default EntryList
+export default EntryList;
